dbee45f27dc041b5c44103294106c55cb065b8e6
Cel A. Skeggs <cela@mit.edu>
2021-08-12 14:45:00 -0400

    Expand scope of scanner

--- ctrl/chart/scan.go
+++ ctrl/chart/scan.go
@@ -22,11 +22,21 @@ type Interval struct {
 	End   model.VirtualTime
 }
 
+type ScannedLine interface {
+	Label() string
+	LastTime() float64
+	BuildPlot(lastTime float64, location float64) *tlplot.TimelinePlot
+}
+
 type ReqScan struct {
 	RequirementName string
 	Failures        []model.VirtualTime
 	Successes       []model.VirtualTime
 	Intervals       []Interval
+}
+
+func (rs *ReqScan) Label() string {
+	return rs.RequirementName
 }
 
 func (rs *ReqScan) LastTime() float64 {
@@ -96,6 +106,9 @@ func ScanRawReqs(path string) (map[string]*ReqScan, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		_ = f.Close()
+	}()
 	cr := csv.NewReader(f)
 	cr.FieldsPerRecord = -1
 
@@ -189,28 +202,225 @@ func ScanRawReqs(path string) (map[string]*ReqScan, error) {
 	}
 }
 
+type InjectionScan struct {
+	Injections []model.VirtualTime
+}
+
+func (i *InjectionScan) Label() string {
+	return "Injections"
+}
+
+func (i *InjectionScan) LastTime() float64 {
+	latest := 0.0
+	for _, injection := range i.Injections {
+		latest = math.Max(latest, injection.Since(model.TimeZero).Seconds())
+	}
+	return latest
+}
+
+func (i *InjectionScan) BuildPlot(lastTime float64, location float64) *tlplot.TimelinePlot {
+	var injections []tlplot.Marker
+
+	injectGlyph := draw.GlyphStyle{
+		Color:  color.RGBA{255, 0, 0, 255},
+		Radius: 8,
+		Shape:  draw.CrossGlyph{},
+	}
+
+	for _, injection := range i.Injections {
+		injections = append(injections, tlplot.Marker{
+			Time:  injection.Since(model.TimeZero).Seconds(),
+			Glyph: injectGlyph,
+		})
+	}
+
+	return tlplot.NewTimelinePlot(nil, injections, location, vg.Points(20))
+}
+
+func ScanInjections(path string) (*InjectionScan, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	cr := csv.NewReader(f)
+	cr.FieldsPerRecord = 5
+
+	firstRow, err := cr.Read()
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return nil, err
+	}
+	if firstRow[1] != "Injection Time" {
+		return nil, fmt.Errorf("invalid first row: %v", firstRow)
+	}
+
+	var scan InjectionScan
+	for {
+		row, err := cr.Read()
+		if err == io.EOF {
+			return &scan, nil
+		} else if err != nil {
+			return nil, err
+		}
+		injectNs, err := strconv.ParseUint(row[1], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		injectV, ok := model.FromNanoseconds(injectNs)
+		if !ok {
+			return nil, fmt.Errorf("invalid timestamp: %v", injectNs)
+		}
+		scan.Injections = append(scan.Injections, injectV)
+	}
+}
+
+type IOScan struct {
+	Reads  []model.VirtualTime
+	Writes []model.VirtualTime
+}
+
+func (i *IOScan) Label() string {
+	return "I/O Events"
+}
+
+func (i *IOScan) LastTime() float64 {
+	latest := 0.0
+	for _, read := range i.Reads {
+		latest = math.Max(latest, read.Since(model.TimeZero).Seconds())
+	}
+	for _, write := range i.Writes {
+		latest = math.Max(latest, write.Since(model.TimeZero).Seconds())
+	}
+	return latest
+}
+
+func (i *IOScan) BuildPlot(lastTime float64, location float64) *tlplot.TimelinePlot {
+	var events []tlplot.Marker
+
+	readGlyph := draw.GlyphStyle{
+		Color:  color.RGBA{0, 128, 255, 255},
+		Radius: 5,
+		Shape:  draw.SquareGlyph{},
+	}
+	writeGlyph := draw.GlyphStyle{
+		Color:  color.RGBA{255, 128, 0, 255},
+		Radius: 5,
+		Shape:  draw.RingGlyph{},
+	}
+
+	for _, event := range i.Reads {
+		events = append(events, tlplot.Marker{
+			Time:  event.Since(model.TimeZero).Seconds(),
+			Glyph: readGlyph,
+		})
+	}
+	for _, event := range i.Writes {
+		events = append(events, tlplot.Marker{
+			Time:  event.Since(model.TimeZero).Seconds(),
+			Glyph: writeGlyph,
+		})
+	}
+
+	return tlplot.NewTimelinePlot(nil, events, location, vg.Points(20))
+}
+
+func ScanIOLog(path string) (*IOScan, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	cr := csv.NewReader(f)
+	cr.FieldsPerRecord = 5
+
+	var scan IOScan
+	for {
+		row, err := cr.Read()
+		if err == io.EOF {
+			return &scan, nil
+		} else if err != nil {
+			return nil, err
+		}
+		// first, we only want to record these as events if they involve I/O
+		transferBytes, err := strconv.ParseUint(row[3], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		if transferBytes == 0 {
+			continue
+		}
+		// determine if read or write
+		var out *[]model.VirtualTime
+		if row[0] == "start" {
+			// transmitted from flight software to simulation
+			out = &scan.Writes
+		} else if row[0] == "end" {
+			// transmitted from simulation to flight software
+			out = &scan.Reads
+		} else {
+			return nil, fmt.Errorf("invalid mode: %q", row[0])
+		}
+		eventNs, err := strconv.ParseUint(row[1], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		eventTime, ok := model.FromNanoseconds(eventNs)
+		if !ok {
+			return nil, fmt.Errorf("invalid timestamp: %v", eventNs)
+		}
+		*out = append(*out, eventTime)
+	}
+}
+
+func ScanAll(dir string) (out []ScannedLine, err error) {
+	records, err := ScanRawReqs(path.Join(dir, "reqs-raw.log"))
+	if err != nil {
+		return nil, err
+	}
+	injections, err := ScanInjections(path.Join(dir, "injections.csv"))
+	if err != nil {
+		return nil, err
+	}
+	ioLog, err := ScanIOLog(path.Join(dir, "timesync.sock.log"))
+	if err != nil {
+		return nil, err
+	}
+	out = []ScannedLine{
+		injections,
+		ioLog,
+	}
+	for _, record := range records {
+		out = append(out, record)
+	}
+	return out, nil
+}
+
 func GeneratePlot(dir string) (*plot.Plot, error) {
 	p := plot.New()
 	p.Title.Text = "Timeline: " + path.Base(dir)
 	p.X.Label.Text = "Virtual Time"
 	p.Y.Label.Text = "Requirement"
 
-	var reqs []string
-
-	records, err := ScanRawReqs(path.Join(dir, "reqs-raw.log"))
+	scans, err := ScanAll(dir)
 	if err != nil {
 		return nil, err
 	}
+
+	sort.Slice(scans, func(i, j int) bool {
+		return scans[i].Label() > scans[j].Label()
+	})
+
 	lastTime := 0.0
-	for req, record := range records {
-		reqs = append(reqs, req)
-		lastTime = math.Max(lastTime, record.LastTime())
+	for _, scan := range scans {
+		lastTime = math.Max(lastTime, scan.LastTime())
 	}
-	sort.Strings(reqs)
-	for i, req := range reqs {
-		p.Add(records[req].BuildPlot(lastTime, float64(i)))
+	var names []string
+	for i, scan := range scans {
+		p.Add(scan.BuildPlot(lastTime, float64(i)))
+		names = append(names, scan.Label())
 	}
-	p.NominalY(reqs...)
+	p.NominalY(names...)
 
 	return p, nil
 }
