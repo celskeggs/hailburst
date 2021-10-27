05ed13d85140469db3bc462c434638693f96d794
Cel A. Skeggs <cela@mit.edu>
2021-10-26 21:45:10 -0400

    Fix atomic declarations

--- fsw/include/hal/atomic.h
+++ fsw/include/hal/atomic.h
@@ -3,8 +3,8 @@
 
 // we can use the builtins available in GCC to make this the same across both FreeRTOS and Linux
 
-#define atomic_load(x) (__atomic_load_n(&(x), __ATOMIC_ACQ_REL))
-#define atomic_store(x, v) (__atomic_store_n(&(x), (v), __ATOMIC_ACQ_REL))
+#define atomic_load(x) (__atomic_load_n(&(x), __ATOMIC_ACQUIRE))
+#define atomic_store(x, v) (__atomic_store_n(&(x), (v), __ATOMIC_RELEASE))
 
 #define atomic_load_relaxed(x) (__atomic_load_n(&(x), __ATOMIC_RELAXED))
 #define atomic_store_relaxed(x, v) (__atomic_store_n(&(x), (v), __ATOMIC_RELAXED))
