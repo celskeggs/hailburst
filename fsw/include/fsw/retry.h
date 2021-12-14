f80cf5375e95f65c5d194a404a4b7f944021b1f6
Cel A. Skeggs <cela@mit.edu>
2021-12-14 15:27:02 -0500

    Fix retry format printing

--- fsw/include/fsw/retry.h
+++ fsw/include/fsw/retry.h
@@ -6,13 +6,13 @@
 
 #include <fsw/debug.h>
 
-#define RETRY(n_retries, ...) \
+#define RETRY(n_retries, fmt, ...) \
     for ( \
         unsigned int _retries = n_retries; \
         _retries > 0; \
         (--_retries) ? \
-            debugf(CRITICAL, "Retrying " __VA_ARGS__) : \
-            debugf(CRITICAL, "After " #n_retries " retries, erroring out during " __VA_ARGS__) \
+            debugf(CRITICAL, "Retrying " fmt, ## __VA_ARGS__) : \
+            debugf(CRITICAL, "After %u retries, erroring out during " fmt, n_retries, ## __VA_ARGS__) \
     )
 
 #endif /* FSW_FAKEWIRE_RETRY_H */
