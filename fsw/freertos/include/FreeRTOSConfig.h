500a3b15237c2b982bfad4f36ed379a0b0857ffd
Cel A. Skeggs <cela@mit.edu>
2022-01-15 18:34:05 -0500

    Eliminate PRIORITY_REPAIR

--- fsw/freertos/include/FreeRTOSConfig.h
+++ fsw/freertos/include/FreeRTOSConfig.h
@@ -8,14 +8,13 @@
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 // configCPU_CLOCK_HZ and configSYSTICK_CLOCK_HZ are not needed for the GCC/ARM_CA9 port.
 #define configTICK_RATE_HZ                      1000
-#define configMAX_PRIORITIES                    3
+#define configMAX_PRIORITIES                    2
 #define configMINIMAL_STACK_SIZE                512
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES   2
 #define configSTACK_DEPTH_TYPE                  uint16_t
 #define configUSE_TASK_FPU_SUPPORT              2
 
 enum {
-    PRIORITY_REPAIR  = 2, // only used for critical repair tasks!
     PRIORITY_WORKERS = 1,
     PRIORITY_IDLE    = 0,
 };
