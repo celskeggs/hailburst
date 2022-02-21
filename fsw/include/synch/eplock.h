d846868f3d0855a1bd3047e45da65e4be5789501
Cel A. Skeggs <cela@mit.edu>
2022-02-21 13:46:18 -0500

    Unified eplock implementations

--- fsw/include/synch/eplock.h
+++ fsw/include/synch/eplock.h
@@ -1,9 +1,9 @@
-#ifndef FSW_FREERTOS_HAL_EPLOCK_H
-#define FSW_FREERTOS_HAL_EPLOCK_H
+#ifndef FSW_SYNCH_EPLOCK_H
+#define FSW_SYNCH_EPLOCK_H
 
 /*
- * This file provides the FreeRTOS implementation of an Epoch Lock. This is a locking mechanism based on the partition
- * scheduler's inherent properties. (Which is emulated by a more ordinary lock on Linux.)
+ * This file provides an implementation of the Epoch Lock. This is a locking mechanism based on the partition
+ * scheduler's inherent properties.
  *
  * Basically: each task must acquire and release the lock *within the same scheduling period*. Rather than actually
  * waiting for the last task to complete, the FreeRTOS implementation will simply *assert* if the last task has not
@@ -20,28 +20,13 @@ typedef struct {
 #define EPLOCK_REGISTER(e_ident)                                                                                      \
     eplock_t e_ident = { .holder = NULL }
 
-#define EPSYNC_ENABLE(e_task) /* allocate nothing on FreeRTOS */
-
 static inline void eplock_acquire(eplock_t *lock) {
     assert(lock != NULL);
     thread_t current_task = task_get_current();
     assert(current_task != NULL);
     thread_t previous_task = atomic_exchange(lock->holder, current_task);
     assertf(previous_task == NULL, "eplock could not be acquired by task %s: task %s failed to meet its deadline",
-            current_task->pcTaskName, previous_task->pcTaskName);
-}
-
-// On Linux, releases the held eplock, waits until another thread acquires and then releases the eplock, and then
-// acquires the eplock. Other calls to eplock_wait_ready are not counted as acquires and releases for the purposes of
-// this function.
-// If this condition is not satisfied within two milliseconds, then false is returned.
-// On FreeRTOS, always returns false immediately.
-// (The duct implementation calls this function when it's waiting on a peer that is not yet done running, and asserts
-//  if false is returned.)
-static inline bool eplock_wait_ready(eplock_t *lock, uint64_t deadline_ns) {
-    assert(lock != NULL);
-    (void) deadline_ns;
-    return false;
+            task_get_name(current_task), task_get_name(previous_task));
 }
 
 static inline void eplock_release(eplock_t *lock) {
@@ -50,7 +35,7 @@ static inline void eplock_release(eplock_t *lock) {
     assert(current_task != NULL);
     thread_t previous_task = atomic_exchange(lock->holder, NULL);
     assertf(previous_task == current_task, "eplock could not be released by task %s: task %s unexpectedly held lock",
-            current_task->pcTaskName, previous_task->pcTaskName);
+            task_get_name(current_task), task_get_name(previous_task));
 }
 
 static inline bool eplock_held(eplock_t *lock) {
@@ -60,4 +45,4 @@ static inline bool eplock_held(eplock_t *lock) {
     return current_task == atomic_load_relaxed(lock->holder);
 }
 
-#endif /* FSW_FREERTOS_HAL_EPLOCK_H */
+#endif /* FSW_SYNCH_EPLOCK_H */
