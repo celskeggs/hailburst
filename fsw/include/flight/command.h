33c94ac591b0485ea20738f62f631b85c8bbc1e4
Cel A. Skeggs <cela@mit.edu>
2022-03-28 14:15:16 -0400

    Fix command clip naming

--- fsw/include/flight/command.h
+++ fsw/include/flight/command.h
@@ -15,11 +15,11 @@ enum {
 // may only be used once
 macro_define(COMMAND_REGISTER, c_ident, c_uplink_pipe) {
     COMM_DEC_REGISTER(symbol_join(c_ident, decoder), c_uplink_pipe, COMMAND_REPLICA_ID);
-    CLIP_REGISTER(symbol_join(c_ident, task), command_execution_clip, &symbol_join(c_ident, decoder))
+    CLIP_REGISTER(symbol_join(c_ident, clip), command_execution_clip, &symbol_join(c_ident, decoder))
 }
 
 macro_define(COMMAND_SCHEDULE, c_ident) {
-    CLIP_SCHEDULE(symbol_join(c_ident, task), 100)
+    CLIP_SCHEDULE(symbol_join(c_ident, clip), 100)
 }
 
 #endif /* FSW_CMD_H */
