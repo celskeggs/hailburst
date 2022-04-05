67a61526ec93f8b16828f7cefac533a9104433ec
Cel A. Skeggs <cela@mit.edu>
2022-04-05 13:13:06 -0400

    Tweak line count script for new python preprocessor structure

--- lc.sh
+++ lc.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e -u
 
-wc -cl $(find *.sh ext/ ctrl/ sim/ fsw/ -type f -not -name '*.o' -not -name 'fakewire_*_test' -not -name 'kernel' -not -name '.sconsign.dblite' -not -path '*build*' -not -name '*-bin' -not -name 'rtos_*.c')
+wc -cl $(find *.sh ext/ ctrl/ sim/ fsw/ -type f      \
+              -not -name '*.o'                       \
+              -not -name 'fakewire_*_test'           \
+              -not -name 'kernel'                    \
+              -not -name '.sconsign.dblite'          \
+              -not -path '*build*'                   \
+              -not -name '*-bin'                     \
+              -not -name 'rtos_*.c'                  \
+              -not -name '*.pyc'                     \
+        ) | sort -n
