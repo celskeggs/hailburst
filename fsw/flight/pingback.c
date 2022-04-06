7ce6100733b59209d1cebaebea1d701593b29ba0
Cel A. Skeggs <cela@mit.edu>
2022-04-05 20:10:08 -0400

    Replicate pingback module

--- fsw/flight/pingback.c
+++ fsw/flight/pingback.c
@@ -4,22 +4,22 @@
 #include <flight/pingback.h>
 #include <flight/telemetry.h>
 
-void pingback_clip(pingback_t *p) {
+void pingback_clip(pingback_replica_t *p) {
     assert(p != NULL);
 
     tlm_txn_t telem;
-    telemetry_prepare(&telem, p->telemetry, PINGBACK_REPLICA_ID);
+    telemetry_prepare(&telem, p->telemetry, p->replica_id);
 
     size_t command_length = 0;
-    uint32_t *command_data = command_receive(p->command, PINGBACK_REPLICA_ID, &command_length);
+    uint32_t *command_data = command_receive(p->command, p->replica_id, &command_length);
     if (command_data != NULL) {
         if (command_length == sizeof(uint32_t)) {
             uint32_t ping_id = be32toh(command_data[0]);
             tlm_pong(&telem, ping_id);
-            command_reply(p->command, PINGBACK_REPLICA_ID, &telem, CMD_STATUS_OK);
+            command_reply(p->command, p->replica_id, &telem, CMD_STATUS_OK);
         } else {
             // wrong length
-            command_reply(p->command, PINGBACK_REPLICA_ID, &telem, CMD_STATUS_UNRECOGNIZED);
+            command_reply(p->command, p->replica_id, &telem, CMD_STATUS_UNRECOGNIZED);
         }
     }
 
--- fsw/include/flight/pingback.h
+++ fsw/include/flight/pingback.h
@@ -7,29 +7,31 @@
 
 #define PINGBACK_REPLICAS 1
 
-enum {
-    PINGBACK_REPLICA_ID = 0,
-};
-
 typedef const struct {
     tlm_endpoint_t *telemetry;
     cmd_endpoint_t *command;
-} pingback_t;
+    uint8_t         replica_id;
+} pingback_replica_t;
 
-void pingback_clip(pingback_t *p);
+void pingback_clip(pingback_replica_t *p);
 
 macro_define(PINGBACK_REGISTER, p_ident) {
     TELEMETRY_ASYNC_REGISTER(symbol_join(p_ident, telemetry), PINGBACK_REPLICAS, 2);
     COMMAND_ENDPOINT(symbol_join(p_ident, command), PING_CID, PINGBACK_REPLICAS);
-    pingback_t p_ident = {
-        .telemetry = &symbol_join(p_ident, telemetry),
-        .command = &symbol_join(p_ident, command),
-    };
-    CLIP_REGISTER(symbol_join(p_ident, clip), pingback_clip, &p_ident)
+    static_repeat(PINGBACK_REPLICAS, p_replica_id) {
+        pingback_replica_t symbol_join(p_ident, replica, p_replica_id) = {
+            .telemetry = &symbol_join(p_ident, telemetry),
+            .command = &symbol_join(p_ident, command),
+        };
+        CLIP_REGISTER(symbol_join(p_ident, clip, p_replica_id),
+                      pingback_clip, &symbol_join(p_ident, replica, p_replica_id));
+    }
 }
 
 macro_define(PINGBACK_SCHEDULE, p_ident) {
-    CLIP_SCHEDULE(symbol_join(p_ident, clip), 10)
+    static_repeat(PINGBACK_REPLICAS, p_replica_id) {
+        CLIP_SCHEDULE(symbol_join(p_ident, clip, p_replica_id), 10)
+    }
 }
 
 macro_define(PINGBACK_TELEMETRY, p_ident) {
