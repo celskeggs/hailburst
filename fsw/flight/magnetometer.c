e7b3cdb45513a328ff43b3c9e1b371a89b788466
Cel A. Skeggs <cela@mit.edu>
2022-04-07 20:04:24 -0400

    Tweak magnetometer to be replicable

--- fsw/flight/magnetometer.c
+++ fsw/flight/magnetometer.c
@@ -25,15 +25,15 @@ enum {
     LATCHING_DELAY_NS = 15 * 1000 * 1000, // wait 15 ms before checking for reading completion
 };
 
-void magnetometer_query_clip(magnetometer_t *mag) {
-    assert(mag != NULL);
+void magnetometer_query_clip(magnetometer_replica_t *mr) {
+    assert(mr != NULL && mr->mut != NULL);
 
     if (clip_is_restart()) {
-        mag->state = MS_INACTIVE;
-        mag->next_reading_time = 0;
-        mag->actual_reading_time = 0;
-        mag->check_latch_time = 0;
-        circ_buf_reset(mag->readings);
+        mr->mut->state = MS_INACTIVE;
+        mr->mut->next_reading_time = 0;
+        mr->mut->actual_reading_time = 0;
+        mr->mut->check_latch_time = 0;
+        circ_buf_reset(mr->readings);
     }
 
     // scratch variables for use in switch statements
@@ -43,15 +43,15 @@ void magnetometer_query_clip(magnetometer_t *mag) {
     uint16_t registers[4];
 
     rmap_txn_t rmap_txn;
-    rmap_epoch_prepare(&rmap_txn, mag->endpoint);
+    rmap_epoch_prepare(&rmap_txn, mr->endpoint);
     tlm_txn_t telem;
-    telemetry_prepare(&telem, mag->telemetry_async, MAGNETOMETER_REPLICA_ID);
+    telemetry_prepare(&telem, mr->telemetry_async, mr->replica_id);
 
-    switch (mag->state) {
+    switch (mr->mut->state) {
     case MS_ACTIVATING:
         status = rmap_write_complete(&rmap_txn, NULL);
         if (status == RS_OK) {
-            mag->state = MS_ACTIVE;
+            mr->mut->state = MS_ACTIVE;
             tlm_mag_pwr_state_changed(&telem, true);
         } else {
             debugf(WARNING, "Failed to turn on magnetometer power, error=0x%03x", status);
@@ -60,7 +60,7 @@ void magnetometer_query_clip(magnetometer_t *mag) {
     case MS_DEACTIVATING:
         status = rmap_write_complete(&rmap_txn, NULL);
         if (status == RS_OK) {
-            mag->state = MS_INACTIVE;
+            mr->mut->state = MS_INACTIVE;
             tlm_mag_pwr_state_changed(&telem, false);
         } else {
             debugf(WARNING, "Failed to turn off magnetometer power, error=0x%03x", status);
@@ -68,12 +68,12 @@ void magnetometer_query_clip(magnetometer_t *mag) {
         break;
     case MS_LATCHING_ON:
         // TODO: stop after a certain number of retries?
-        mag->actual_reading_time = 0;
-        status = rmap_write_complete(&rmap_txn, &mag->actual_reading_time);
+        mr->mut->actual_reading_time = 0;
+        status = rmap_write_complete(&rmap_txn, &mr->mut->actual_reading_time);
         if (status == RS_OK) {
-            assert(mag->actual_reading_time != 0);
-            mag->state = MS_LATCHED_ON;
-            mag->check_latch_time = timer_now_ns() + LATCHING_DELAY_NS;
+            assert(mr->mut->actual_reading_time != 0);
+            mr->mut->state = MS_LATCHED_ON;
+            mr->mut->check_latch_time = timer_now_ns() + LATCHING_DELAY_NS;
         } else {
             debugf(WARNING, "Failed to turn on magnetometer latch, error=0x%03x", status);
         }
@@ -86,15 +86,15 @@ void magnetometer_query_clip(magnetometer_t *mag) {
                 registers[i] = be16toh(registers[i]);
             }
             if (registers[0] == LATCH_OFF) {
-                tlm_mag_reading_t *reading = circ_buf_write_peek(mag->readings, 0);
+                tlm_mag_reading_t *reading = circ_buf_write_peek(mr->readings, 0);
                 if (reading != NULL) {
-                    reading->reading_time = clock_mission_adjust(mag->actual_reading_time);
+                    reading->reading_time = clock_mission_adjust(mr->mut->actual_reading_time);
                     reading->mag_x = registers[REG_X - REG_LATCH];
                     reading->mag_y = registers[REG_Y - REG_LATCH];
                     reading->mag_z = registers[REG_Z - REG_LATCH];
-                    circ_buf_write_done(mag->readings, 1);
+                    circ_buf_write_done(mr->readings, 1);
                 }
-                mag->state = MS_ACTIVE;
+                mr->mut->state = MS_ACTIVE;
             }
             // otherwise keep checking until latch turns off
         } else {
@@ -107,39 +107,39 @@ void magnetometer_query_clip(magnetometer_t *mag) {
     }
 
     size_t command_length = 0;
-    uint8_t *command_bytes = command_receive(mag->command_endpoint, MAGNETOMETER_REPLICA_ID, &command_length);
+    uint8_t *command_bytes = command_receive(mr->command_endpoint, mr->replica_id, &command_length);
     if (command_bytes != NULL) {
         if (command_length == 1 && (command_bytes[0] == 0 || command_bytes[0] == 1)) {
-            mag->should_be_powered = (command_bytes[0] == 1);
-            debugf(DEBUG, "Command set magnetometer power state to %u.", mag->should_be_powered);
-            command_reply(mag->command_endpoint, MAGNETOMETER_REPLICA_ID, &telem, CMD_STATUS_OK);
+            mr->mut->should_be_powered = (command_bytes[0] == 1);
+            debugf(DEBUG, "Command set magnetometer power state to %u.", mr->mut->should_be_powered);
+            command_reply(mr->command_endpoint, mr->replica_id, &telem, CMD_STATUS_OK);
         } else {
             // wrong length or invalid byte
-            command_reply(mag->command_endpoint, MAGNETOMETER_REPLICA_ID, &telem, CMD_STATUS_UNRECOGNIZED);
+            command_reply(mr->command_endpoint, mr->replica_id, &telem, CMD_STATUS_UNRECOGNIZED);
         }
     }
 
-    if ((mag->state == MS_INACTIVE || mag->state == MS_DEACTIVATING) && mag->should_be_powered) {
+    if ((mr->mut->state == MS_INACTIVE || mr->mut->state == MS_DEACTIVATING) && mr->mut->should_be_powered) {
         debugf(DEBUG, "Turning on magnetometer power...");
-        mag->state = MS_ACTIVATING;
-    } else if ((mag->state == MS_ACTIVATING || mag->state == MS_ACTIVE) && !mag->should_be_powered) {
+        mr->mut->state = MS_ACTIVATING;
+    } else if ((mr->mut->state == MS_ACTIVATING || mr->mut->state == MS_ACTIVE) && !mr->mut->should_be_powered) {
         debugf(DEBUG, "Turning off magnetometer power...");
-        mag->state = MS_DEACTIVATING;
-    } else if (mag->state == MS_ACTIVE && timer_epoch_ns() >= mag->next_reading_time) {
+        mr->mut->state = MS_DEACTIVATING;
+    } else if (mr->mut->state == MS_ACTIVE && timer_epoch_ns() >= mr->mut->next_reading_time) {
         debugf(DEBUG, "Taking magnetometer reading...");
-        mag->state = MS_LATCHING_ON;
-        mag->next_reading_time += READING_DELAY_NS;
-    } else if (mag->state == MS_LATCHED_ON && timer_now_ns() >= mag->check_latch_time) {
-        mag->state = MS_TAKING_READING;
+        mr->mut->state = MS_LATCHING_ON;
+        mr->mut->next_reading_time += READING_DELAY_NS;
+    } else if (mr->mut->state == MS_LATCHED_ON && timer_now_ns() >= mr->mut->check_latch_time) {
+        mr->mut->state = MS_TAKING_READING;
     }
 
-    switch (mag->state) {
+    switch (mr->mut->state) {
     case MS_ACTIVATING:
         single_value = htobe16(POWER_ON);
         rmap_write_start(&rmap_txn, 0x00, REG_POWER, (uint8_t*) &single_value, sizeof(single_value));
         // we set this here -- rather than next cycle -- so that we can avoid the single-epoch discrepancy a delay
         // would imply
-        mag->next_reading_time = timer_epoch_ns() + READING_DELAY_NS;
+        mr->mut->next_reading_time = timer_epoch_ns() + READING_DELAY_NS;
         break;
     case MS_DEACTIVATING:
         single_value = htobe16(POWER_OFF);
@@ -164,45 +164,45 @@ void magnetometer_query_clip(magnetometer_t *mag) {
     rmap_epoch_commit(&rmap_txn);
 }
 
-static void magnetometer_telem_iterator_fetch(void *mag_opaque, size_t index, tlm_mag_reading_t *reading_out) {
-    magnetometer_t *mag = (magnetometer_t *) mag_opaque;
-    assert(mag != NULL && reading_out != NULL);
+static void magnetometer_telem_iterator_fetch(void *mr_opaque, size_t index, tlm_mag_reading_t *reading_out) {
+    magnetometer_replica_t *mr = (magnetometer_replica_t *) mr_opaque;
+    assert(mr != NULL && reading_out != NULL);
 
-    tlm_mag_reading_t *reading = circ_buf_read_peek(mag->readings, index);
+    tlm_mag_reading_t *reading = circ_buf_read_peek(mr->readings, index);
     assert(reading != NULL);
     *reading_out = *reading;
 }
 
-void magnetometer_telem_clip(magnetometer_t *mag) {
-    assert(mag != NULL);
+void magnetometer_telem_clip(magnetometer_replica_t *mr) {
+    assert(mr != NULL && mr->mut != NULL);
 
     local_time_t now = timer_now_ns();
 
     if (clip_is_restart()) {
-        circ_buf_reset(mag->readings);
+        circ_buf_reset(mr->readings);
         // make sure this can't get corrupted to a value that prevents us from sending telemetry
-        if (mag->last_telem_time > now) {
-            mag->last_telem_time = now;
+        if (mr->mut->last_telem_time > now) {
+            mr->mut->last_telem_time = now;
         }
     }
 
     tlm_txn_t telem;
-    telemetry_prepare(&telem, mag->telemetry_sync, MAGNETOMETER_REPLICA_ID);
+    telemetry_prepare(&telem, mr->telemetry_sync, mr->replica_id);
 
-    circ_index_t downlink_count = circ_buf_read_avail(mag->readings);
+    circ_index_t downlink_count = circ_buf_read_avail(mr->readings);
     if (downlink_count == 0) {
         // nothing to downlink, so a send is unnecessary.
-        mag->last_telem_time = now;
+        mr->mut->last_telem_time = now;
     } else {
         // something to downlink... but only downlink at most every 5.5 seconds (to meet requirements), and only if
         // there's room in the telemetry buffer to actually transmit data.
-        if (now >= mag->last_telem_time + (uint64_t) 5500 * CLOCK_NS_PER_MS && telemetry_can_send(&telem)) {
+        if (now >= mr->mut->last_telem_time + (uint64_t) 5500 * CLOCK_NS_PER_MS && telemetry_can_send(&telem)) {
             size_t write_count = downlink_count;
-            tlm_mag_readings_map(&telem, &write_count, magnetometer_telem_iterator_fetch, mag);
+            tlm_mag_readings_map(&telem, &write_count, magnetometer_telem_iterator_fetch, (void *) mr);
             assert(write_count >= 1 && write_count <= downlink_count);
-            circ_buf_read_done(mag->readings, write_count);
+            circ_buf_read_done(mr->readings, write_count);
 
-            mag->last_telem_time = now;
+            mr->mut->last_telem_time = now;
         }
     }
 
--- fsw/include/flight/magnetometer.h
+++ fsw/include/flight/magnetometer.h
@@ -11,7 +11,6 @@
 #include <flight/telemetry.h>
 
 #define MAGNETOMETER_REPLICAS 1
-#define MAGNETOMETER_REPLICA_ID 0
 
 enum {
     MAGNETOMETER_MAX_READINGS = 100,
@@ -27,55 +26,69 @@ enum magnetometer_state {
     MS_DEACTIVATING,
 };
 
-typedef struct {
-    rmap_replica_t *endpoint;
+typedef const struct {
+    struct magnetometer_replica_mut {
+        // synchronization
+        bool should_be_powered;
 
-    // synchronization
-    bool should_be_powered;
+        // state saved between query clip invocations
+        enum magnetometer_state state;
+        local_time_t next_reading_time;
+        local_time_t actual_reading_time;
+        local_time_t check_latch_time;
+
+        // state saved between telemetry clip invocations
+        local_time_t last_telem_time;
+    } *mut;
+
+    uint8_t replica_id;
+
+    // spacecraft bus connection
+    rmap_replica_t *endpoint;
 
     // telemetry buffer
     circ_buf_t *readings;
-
-    // state saved between query clip invocations
-    enum magnetometer_state state;
-    local_time_t next_reading_time;
-    local_time_t actual_reading_time;
-    local_time_t check_latch_time;
-
-    // state saved between telemetry clip invocations
-    local_time_t last_telem_time;
 
     // telemetry and command endpoints
     tlm_endpoint_t *telemetry_async;
     tlm_endpoint_t *telemetry_sync;
     cmd_endpoint_t *command_endpoint;
-} magnetometer_t;
+} magnetometer_replica_t;
 
-void magnetometer_query_clip(magnetometer_t *mag);
-void magnetometer_telem_clip(magnetometer_t *mag);
+void magnetometer_query_clip(magnetometer_replica_t *mag);
+void magnetometer_telem_clip(magnetometer_replica_t *mag);
 
 macro_define(MAGNETOMETER_REGISTER, m_ident, m_address, m_switch_in, m_switch_out, m_switch_port) {
-    CIRC_BUF_REGISTER(symbol_join(m_ident, readings), sizeof(tlm_mag_reading_t), MAGNETOMETER_MAX_READINGS);
     TELEMETRY_ASYNC_REGISTER(symbol_join(m_ident, telemetry_async), MAGNETOMETER_REPLICAS, 2);
     TELEMETRY_SYNC_REGISTER(symbol_join(m_ident, telemetry_sync), MAGNETOMETER_REPLICAS, 1);
     COMMAND_ENDPOINT(symbol_join(m_ident, command), MAG_SET_PWR_STATE_CID, MAGNETOMETER_REPLICAS);
     RMAP_ON_SWITCHES(symbol_join(m_ident, endpoint), MAGNETOMETER_REPLICAS, m_switch_in, m_switch_out,
                      m_switch_port, m_address, 8, 4);
-    magnetometer_t m_ident = {
-        .endpoint = RMAP_REPLICA_REF(symbol_join(m_ident, endpoint), MAGNETOMETER_REPLICA_ID),
-        .should_be_powered = false,
-        .readings = &symbol_join(m_ident, readings),
-        .state = MS_INACTIVE,
-        .next_reading_time = 0,
-        .actual_reading_time = 0,
-        .check_latch_time = 0,
-        .last_telem_time = 0,
-        .telemetry_async = &symbol_join(m_ident, telemetry_async),
-        .telemetry_sync = &symbol_join(m_ident, telemetry_sync),
-        .command_endpoint = &symbol_join(m_ident, command),
-    };
-    CLIP_REGISTER(symbol_join(m_ident, query), magnetometer_query_clip, &m_ident);
-    CLIP_REGISTER(symbol_join(m_ident, telem), magnetometer_telem_clip, &m_ident)
+    static_repeat(MAGNETOMETER_REPLICAS, m_replica_id) {
+        CIRC_BUF_REGISTER(symbol_join(m_ident, readings, m_replica_id),
+                          sizeof(tlm_mag_reading_t), MAGNETOMETER_MAX_READINGS);
+        struct magnetometer_replica_mut symbol_join(m_ident, mutable, m_replica_id) = {
+            .should_be_powered = false,
+            .state = MS_INACTIVE,
+            .next_reading_time = 0,
+            .actual_reading_time = 0,
+            .check_latch_time = 0,
+            .last_telem_time = 0,
+        };
+        magnetometer_replica_t symbol_join(m_ident, replica, m_replica_id) = {
+            .mut = &symbol_join(m_ident, mutable, m_replica_id),
+            .replica_id = m_replica_id,
+            .endpoint = RMAP_REPLICA_REF(symbol_join(m_ident, endpoint), m_replica_id),
+            .readings = &symbol_join(m_ident, readings, m_replica_id),
+            .telemetry_async = &symbol_join(m_ident, telemetry_async),
+            .telemetry_sync = &symbol_join(m_ident, telemetry_sync),
+            .command_endpoint = &symbol_join(m_ident, command),
+        };
+        CLIP_REGISTER(symbol_join(m_ident, query, m_replica_id),
+                      magnetometer_query_clip, &symbol_join(m_ident, replica, m_replica_id));
+        CLIP_REGISTER(symbol_join(m_ident, telem, m_replica_id),
+                      magnetometer_telem_clip, &symbol_join(m_ident, replica, m_replica_id));
+    }
 }
 
 // one RMAP channel
@@ -85,8 +98,10 @@ macro_define(MAGNETOMETER_REGISTER, m_ident, m_address, m_switch_in, m_switch_ou
 #define MAGNETOMETER_MAX_IO_PACKET     RMAP_MAX_IO_PACKET(8, 4)
 
 macro_define(MAGNETOMETER_SCHEDULE, m_ident) {
-    CLIP_SCHEDULE(symbol_join(m_ident, query), 20)
-    CLIP_SCHEDULE(symbol_join(m_ident, telem), 100)
+    static_repeat(MAGNETOMETER_REPLICAS, m_replica_id) {
+        CLIP_SCHEDULE(symbol_join(m_ident, query, m_replica_id), 20)
+        CLIP_SCHEDULE(symbol_join(m_ident, telem, m_replica_id), 100)
+    }
 }
 
 macro_define(MAGNETOMETER_TELEMETRY, m_ident) {
